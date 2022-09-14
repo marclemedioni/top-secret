493748612193c1add76484801822d5cf9da0ea3b
Marc <marc@docorga.com>
2022-09-14 09:35:28 +0200

    bootstrap front

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { ChatsController, ChatsService } from '@top-secret/chats'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatsController, ChatsService } from '@top-secret/chats'
 
 @Module({
   imports: [],
--- libs/ui/src/lib/chat.service.ts
+++ libs/ui/src/lib/chat.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Chat } from '@prisma/client'
+import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ChatService {
+export class ProductService {
+  private API_URL = 'http://localhost:3333/api'
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  public getProducts(): Observable<Chat[]> {
+    {
+      return this.http.get<Chat[]>(`${this.API_URL}/chats`)
+    }
+  }
 }
