ac04e38a16f3e7731959a9dd6f1bf58a3ef5dc39
Marc <marc@docorga.com>
2022-09-14 10:16:36 +0200

    (front) Create login/register components

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { ChatsComponent } from './chats/chats.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [ChatsComponent],
+  declarations: [ChatsComponent, LoginComponent, RegisterComponent],
   exports: [ChatsComponent],
 })
 export class UiModule {}
