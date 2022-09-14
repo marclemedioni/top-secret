4106deb9c568a850bceb2da651e2d3998cc9b1fe
Marc <marc@docorga.com>
2022-09-14 08:54:33 +0200

    (front) create chats component

---
+++ libs/ui/src/lib/chats/chats.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'top-secret-chats',
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.scss'],
+})
+export class ChatsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatsComponent } from './chats/chats.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ChatsComponent],
+  exports: [ChatsComponent],
 })
 export class UiModule {}
