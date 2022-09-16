a66f16fb07e32b654d76acdc7bead32d88e7f1cf
Marc <marc@docorga.com>
2022-09-16 14:16:55 +0200

    (e2e) Add hints for login-page tests

--- apps/top-secret-e2e/src/support/login-page.po.ts
+++ apps/top-secret-e2e/src/support/login-page.po.ts
@@ -1,1 +1,4 @@
 export const getUsernameInput = () => cy.get("[data-cy='usernameInput']");
+export const getPasswordInput = () => cy.get("[data-cy='passwordInput']");
+export const getRemmberMeCheckbox = () => cy.get("[data-cy='rememberMeCheckbox']")
+export const getSubmitButton = () => cy.get("[data-cy='submitButton']")
